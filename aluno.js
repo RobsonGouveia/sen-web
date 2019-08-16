6d83390a42195119f1f32e5c3cfe03aa4740d744
ROBSON BEZERRA DE GOUVEIA JUNIOR <387778627@alunos.etec.itaqua>
2019-08-16 15:23:42 -0300

    Foi criado arquivo JS para validação de formulario de aluno e importação de ladda-button

---
+++ aluno.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    console.warn("OLAAAA MUNDO")
+
+    let meuForm = $("form#formAluno")
+
+    console.log(meuForm)
+
+    meuForm.on("submit", function(event){
+        event.preventDefault()
+        console.log("DEU CERTO")
+        let botao = $("form button")
+        console.log(botao)
+
+        let botaoQGira = Ladda.create(botao[0])
+        botaoQGira.start()
+    }) 
+
+}
