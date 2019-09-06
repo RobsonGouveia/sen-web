2ccc839eb5136fc7e278f93e7192764b7a7f70d6
ROBSON BEZERRA DE GOUVEIA JUNIOR <387778627@alunos.etec.itaqua>
2019-09-06 15:21:42 -0300

    Foi realizado requisição assincrona com servidor de API

--- aluno.js
+++ aluno.js
@@ -13,6 +13,11 @@ window.onload = function() {
 
         let botaoQGira = Ladda.create(botao[0])
         botaoQGira.start()
+        fetch("http://localhost:8080/sen-api/teste.php").then(function(resposta){
+        return resposta.json()
+        }).then(function(dados){
+            alert(dados.mensagem)
+        }) 
     }) 
 
 }
